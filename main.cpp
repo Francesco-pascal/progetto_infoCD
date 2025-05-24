00379969178d35045c579bf7a8974438bacda90f
Francesco-pascal <francesco.dilorenzo@studenti.iispascal.it>
2025-05-24 16:56:57 +0200

    punti 5 e 6, 7 saltato e iniziato l'8

--- main.cpp
+++ main.cpp
@@ -21,31 +21,40 @@ struct desc_corso{
 map<string,string> corsi_per_matricola;
 map<string,string> corsi_per_cognome;
 map<string,vector<studente>> studenti_per_corso;
-map<string,vector<studente>> esami_per_corso;
-/*map<string,map<string,int>> num_studenti_per_corso;
-map<string,int> num_materie_per_corso;
-map<string,vector<string>> materie_per_descrizione;*/
+map<string,vector<materia>> esami_per_corso;
 
 
 void menu(){
-    cout<<"=====Gestione universitaria====="<<endl;
-    cout<<"0.Caricamento dati da un file"<<endl;
-    cout<<"1.Cerca corsi di uno studente(matricola)"<<endl;
-    cout<<"2.Cerca corsi di uno studente(cognome)"<<endl;
-    cout<<"3.Elenca studenti iscritti ad un corso"<<endl;
-    cout<<"4.Stampa dati esami di un corso"<<endl;
-    cout<<"5.Numero di studenti per corso"<<endl;
-    cout<<"6.Numero di materie per corso"<<endl;
-    cout<<"7.Cerca materia per descrizione"<<endl;
-    cout<<"8.Inserisci nuovo studente"<<endl;
-    cout<<"9.Salva i dati su file"<<endl;
-    cout<<"X.Esci"<<endl;
+    cout<<"==============Gestione universitaria=============="<<endl;
+    cout<<"=                                                ="<<endl;
+    cout<<"=    0. Caricamento dati da un file              ="<<endl;
+    cout<<"=    1. Cerca corsi di uno studente(matricola)   ="<<endl;
+    cout<<"=    2. Cerca corsi di uno studente(cognome)     ="<<endl;
+    cout<<"=    3. Elenca studenti iscritti ad un corso     ="<<endl;
+    cout<<"=    4. Stampa dati esami di un corso            ="<<endl;
+    cout<<"=    5. Numero di studenti per corso             ="<<endl;
+    cout<<"=    6. Numero di materie per corso              ="<<endl;
+    cout<<"=    7. Cerca materia per descrizione            ="<<endl;
+    cout<<"=    8. Inserisci nuovo studente                 ="<<endl;
+    cout<<"=    9. Salva i dati su file                     ="<<endl;
+    cout<<"=    X. Esci                                     ="<<endl;
+    cout<<"=                                                ="<<endl;
+    cout<<"=================================================="<<endl;
 
 }
 
 bool studenteGiaPresente(vector<studente> &v, studente &stu) {
-    for(auto& s : v) {
-        if(s.matricola_studente == stu.matricola_studente) {
+    for(auto& elem : v) {
+        if(elem.matricola_studente == stu.matricola_studente) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool materiaGiaPresente(vector<materia> &v, materia &mat) {
+    for(auto& elem : v) {
+        if(elem.descrizione_materia == mat.descrizione_materia) {
             return true;
         }
     }
@@ -85,15 +94,11 @@ void leggiCSV(vector<studente> &s, vector<materia> &m, vector<desc_corso> &dc){
         if(!studenteGiaPresente(studenti_per_corso[desc.codice_corso], stu))    studenti_per_corso[desc.codice_corso].push_back(stu);
 
         //punto 4
-        esami_per_corso[mat.codice_materia].push_back(stu);
-
-
+        if(!materiaGiaPresente(esami_per_corso[desc.codice_corso], mat))    esami_per_corso[desc.codice_corso].push_back(mat);
 
     }
     fin.close();
 }
-
-
 
 int main()
 {
@@ -108,7 +113,7 @@ int main()
     vector<desc_corso> desc;
 
 
-    while(scelta!='x'){
+    while(scelta!='x' and scelta!='X'){
         switch(scelta){
             case '0':{
                 leggiCSV(stud,mat,desc);
@@ -123,7 +128,7 @@ int main()
 
                 break;
             }
-            case '2':{
+            case '2':{ //ricontrollare i cognomi, forse è da fare una map<string,vector<string>>
                 string corso;
                 cout<<"Inserisci il cognome da cercare: ";
                 cin>>corso;
@@ -141,21 +146,29 @@ int main()
                 break;
             }
             case '4':{
-                    string esame;
-                    cout<<"Inserisci l'esame del corso: ";
-                    cin>>esame;
+                string corso;
+                cout<<"Inserisci l'esame del corso: ";
+                cin>>corso;
 
-                    for(auto elem : esami_per_corso[esame]){
-                        cout<<esame<<" : "<< elem.matricola_studente << " " << elem.cognome_studente << " " << elem.nome_studente << endl;
-                    }
+                for(auto elem : esami_per_corso[corso]){
+                    cout<<corso<<" : "<< elem.descrizione_materia << endl;
+                }
                 break;
             }
             case '5':{
+                string corso;
+                cout<<"Inserisci il corso: ";
+                cin>>corso;
 
+                cout<<"il corso di "<<corso<<" ha "<<studenti_per_corso[corso].size()<<" studenti "<<endl;
                 break;
             }
             case '6':{
+                string corso;
+                cout<<"Inserisci il corso: ";
+                cin>>corso;
 
+                cout<<"Il corso di "<<corso<<" ha "<<esami_per_corso[corso].size()<<" corsi "<<endl;
                 break;
             }
             case '7':{
@@ -163,6 +176,29 @@ int main()
                 break;
             }
             case '8':{
+                studente s;
+                materia m;
+                desc_corso c;
+
+                cout<<"Inserisci il codice del corso: "<<endl;
+                cin>>c.codice_corso;
+                cout<<"Inserisci la descizione del corso: "<<endl;
+                cin>>c.descrizione_corso;
+                desc.push_back(c);
+
+                cout<<"Inserisci il codice della materia: "<<endl;
+                cin>>m.codice_materia;
+                cout<<"Inserisci la descrizione della materia: "<<endl;
+                cin>>m.descrizione_materia;
+                mat.push_back(m);
+
+                cout<<"Inserisci la matricola: "<<endl;
+                cin>>s.matricola_studente;
+                cout<<"Inserisci il cognome: "<<endl;
+                cin>>s.cognome_studente;
+                cout<<"Inserisci il nome: "<<endl;
+                cin>>s.nome_studente;
+                stud.push_back(s);
 
                 break;
             }
